b0c15dc50bd2334b2097e8d38d55ea6fbcf783db
Fabio Petrone <fa610movic90@gmail.com>
2021-02-26 16:06:05 +0100

    ordinati file con obiettivo

--- bonus/index.php
+++ bonus/index.php
@@ -5,7 +5,17 @@
   <title>basket</title>
 </head>
 <body>
-  <?php
+<?php
+
+/*Creiamo un array 'matches' contenente altri array i
+quali rappresentano delle partite di basket di
+un’ipotetica tappa del calendario. Ogni array della
+partita avrà una squadra di casa e una squadra
+ospite, punti fatti dalla squadra di casa e punti fatti
+dalla squadra ospite.
+Stampiamo a schermo tutte le partite con questo
+schema:
+Olimpia Milano - Cantù | 55 - 60*/
 
 $matches = [
   [
--- email/index.php
+++ email/index.php
@@ -1,4 +1,14 @@
   <?php
+// Obiettivo
+  // Passare come parametri GET name, mail e age e
+  // verificare (cercando i metodi che non
+  // conosciamo nella documentazione) che:
+  // 1. name sia più lungo di 3 caratteri,
+  // 2. mail contenga un punto e una chiocciola
+  // 3. age sia un numero.
+  // Se tutto è ok stampare “Accesso riuscito”, altrimenti
+  // “Accesso negato”.
+
     // if ($_GET) {
     //   echo 'Ci sono elementi in get';
     // } else {
